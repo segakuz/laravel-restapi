b04e883af1efb0de3cd06f3f6682a50a7f4db15a
sergei kuznetsov <segakuz@gmail.com>
2019-11-01 12:00:40 +0500

    + use authcontroller in api route

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\GameController;
+use App\Http\Controllers\Api\AuthController;
 
 /*
 |--------------------------------------------------------------------------
